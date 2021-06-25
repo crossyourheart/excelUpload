62778932cf3114dae4bb5f34e4a41bb66fb37729
wangwei <709023963@qq.com>
2021-06-25 14:47:07 +0800

    Initial commit

---
+++ src/main/java/com/example/excelupload/demo/DemoController.java
@@ -0,0 +1,44 @@
+package com.example.excelupload.demo;
+
+import com.alibaba.excel.EasyExcel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * @author wang wei
+ * @description
+ * @create 2021/6/25 13:52
+ */
+@Controller
+public class DemoController {
+
+    @Autowired
+    DemoDAO demoDAO;
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
+
+    /**
+     * 文件上传
+     * <p>
+     * 1. 创建excel对应的实体对象
+     * <p>
+     * 2. 由于默认一行行的读取excel，所以需要创建excel一行一行的回调监听器
+     * <p>
+     * 3. 直接读即可
+     */
+    @PostMapping("upload")
+    @ResponseBody
+    public String upload(MultipartFile file) throws IOException {
+        EasyExcel.read(file.getInputStream(), DemoData.class, new DemoDataListener(demoDAO)).sheet().doRead();
+        return "success";
+    }
+}
---
+++ src/main/java/com/example/excelupload/demo/DemoData.java
@@ -0,0 +1,18 @@
+package com.example.excelupload.demo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author wang wei
+ * @description
+ * @create 2021/6/25 13:53
+ */
+
+@Data
+public class DemoData {
+   private String string;
+   private Date date;
+   private Double doubleData;
+}
